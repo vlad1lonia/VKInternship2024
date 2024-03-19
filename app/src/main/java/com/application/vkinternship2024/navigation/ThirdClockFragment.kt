8add38abbe310e9b728be148bb04e5d3aa482d8a
Vladislav <vladbalandin2013@gmail.com>
2024-03-19 22:29:44 +0300

    Minor changes

--- app/src/main/java/com/application/vkinternship2024/navigation/ThirdClockFragment.kt
+++ app/src/main/java/com/application/vkinternship2024/navigation/ThirdClockFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.application.vkinternship2024.R
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -18,7 +17,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ThirdClockFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -47,7 +45,6 @@ class ThirdClockFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ThirdClockFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ThirdClockFragment().apply {
